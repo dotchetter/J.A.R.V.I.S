4aee4d19b7ebe7607692666e35197d22a3ab8d7b
dotchetter <dotchetter@protonmail.ch>
2020-07-17 12:47:55 +0200

    Trying out TimeKeeperFeature

--- src/exec.py
+++ src/exec.py
@@ -2,18 +2,20 @@ import CommandIntegrator as ci
 import discordInterface
 import configparser as cp
 from features.echofeature import EchoFeature
+from features.timekeeperfeature import TimeKeeperFeature
 
 if __name__ == '__main__':
 
 	config_parser = cp.ConfigParser()
 	config_parser.read('config.ini')
 
-	echo_feature = EchoFeature()
+	echo = EchoFeature()
+	timekeeper = TimeKeeperFeature()
 
 	processor = ci.CommandProcessor()
 	interface = discordInterface.discordInterface()
 
-	processor.features = echo_feature
+	processor.features = (echo, timekeeper)
 	interface.command_processor = processor
 	interface.guild = config_parser['DISCORD']['guild']
 
