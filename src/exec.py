cce9482232e77a6575df3bf11e5e563d99d1e7aa
Simon Olofsson <dotchetter@protonmail.ch>
2020-10-21 04:36:49 +0200

    Added debug mode where Jarvis is not connecting to discord and listens to speech using google speech api.

--- src/exec.py
+++ src/exec.py
@@ -1,18 +1,52 @@
+import argparse
+import configparser as cp
+
 import commandintegrator as ci
 import discordInterface
-import configparser as cp
 from features.smarthomefeature import SmartHomeFeature
 
 if __name__ == '__main__':
 
-	config_parser = cp.ConfigParser()
-	config_parser.read('config.ini')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-debug", action="store_true")
 
-	processor = ci.CommandProcessor()
-	interface = discordInterface.discordInterface()
+    args = parser.parse_args()
 
-	processor.features = SmartHomeFeature()
-	interface.command_processor = processor
-	interface.guild = config_parser['DISCORD']['guild']
+    config_parser = cp.ConfigParser()
+    config_parser.read('config.ini')
 
-	interface.run(config_parser['DISCORD']['token'])
+    processor = ci.CommandProcessor()
+    interface = discordInterface.discordInterface()
+
+    processor.features = SmartHomeFeature()
+    interface.command_processor = processor
+    interface.guild = config_parser['DISCORD']['guild']
+
+    if args.debug:
+
+        import speech_recognition as sr
+        r = sr.Recognizer()
+        message = ci.Message()
+
+        print("J.A.R.V.I.S debug console",
+              "Version: 0.2.4 A\n\n",
+              f"Features loaded: {[i for i in processor.features]}",
+              "\n -> JARVIS is ready.", sep="\n")
+
+        while True:
+            with sr.Microphone() as source:
+                audio = r.listen(source)
+
+                try:
+                    recognized = r.recognize_google(audio)
+                except sr.UnknownValueError:
+                    continue
+
+                print("You:", recognized)
+                message.content = recognized
+
+            response = processor.process(message)
+            print(f"\nJarvis: {response.response()}")
+
+    else:
+        interface.run(config_parser['DISCORD']['token'])
